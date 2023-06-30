4cd1d1283e413281405bf66708c7046228881085
DELL <crazy_63@mail.ru>
2023-06-30 14:47:27 +0300

    up

---
+++ Vector2D.cs
@@ -0,0 +1,40 @@
+﻿namespace Task_Test
+{
+    internal class Vector2D
+    {
+        public float X { get; }
+        public float Y { get; }
+
+        public Vector2D(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Vector2D(float value)
+        {
+            X = value;
+            Y = value;
+        }
+
+        public static Vector2D operator +(Vector2D vectorA, Vector2D vectorB)
+        {
+            return new Vector2D(vectorA.X + vectorB.X, vectorA.Y + vectorB.Y);
+        }
+
+        public static Vector2D operator -(Vector2D vectorA, Vector2D vectorB)
+        {
+            return new Vector2D(vectorA.X - vectorB.X, vectorA.Y - vectorB.Y);
+        }
+
+        public static Vector2D operator *(Vector2D vectorA, Vector2D vectorB)
+        {
+            return new Vector2D(vectorA.X * vectorB.X, vectorA.Y * vectorB.Y);
+        }
+
+        public static Vector2D operator /(Vector2D vectorA, Vector2D vectorB)
+        {
+            return new Vector2D(vectorA.X / vectorB.Y, vectorA.X / vectorB.Y);
+        }
+    }
+}
